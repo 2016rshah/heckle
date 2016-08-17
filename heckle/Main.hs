cbd114bf423af125736b2cb1a333c02a4e80966b
Matt Chan <mattchan@awakenetworks.com>
2016-08-17 11:26:25 -0700

    ok it builds

--- heckle/Main.hs
+++ heckle/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Exception
 import Data.Either
 import Data.List
+import Data.Maybe
 import System.Directory
 import System.Environment (getArgs)
 import System.Process     (readProcess)
@@ -16,13 +17,13 @@ main = do
     ["build"] -> do
 
       putStrLn "Reading directory and turning into native posts"
-      postsToBeCreated <- mapM fileToPost =<< (getDirectoryContents "posts")
-      let posts = (reverse . sort . rights) (postsToBeCreated)
-      putStrLn ("Number of posts found: " ++ (show (length posts)))
+      postsToBeCreated <- mapM fileToPost =<< getDirectoryContents "posts"
+      let posts = reverse . sort . rights $ postsToBeCreated
+      putStrLn $ "Number of posts found: " ++ show (length posts)
 
       putStrLn "Writing markdown files into template HTML"
       template <- readFile "template.html.hkl"
-      mapM_ (writeHTML template) posts
+      sequence $ mapMaybe (writeHTML template) posts
 
       putStrLn "Creating HTML <ul> element for index file"
       let generatedHtml = postsToHtml posts
@@ -31,32 +32,33 @@ main = do
       layoutFile <- readFile "index.html.hkl"
       let injectedOutput = layoutFile `injectIndex` generatedHtml
       case injectedOutput of
-        (Left e) -> putStrLn e
-        (Right s) -> do
+        Nothing -> putStrLn "Error with templating"
+        Just s  -> do
           writeFile "index.html" s
           putStrLn "Success building!"
 
 
     ["init"] -> do
-      --Create the basic layout file
-      writeFile "index.html.hkl" exampleIndexFile --Change to layout when testing, index when deploying"
+      -- Create the basic layout file
+      writeFile "index.html.hkl" exampleIndexFile -- Change to layout when testing, index when deploying"
       writeFile "template.html.hkl" exampleTemplateFile
       writeFile "index.html" exampleResFile
-      --Create directory for posts and basic post
+
+      -- Create directory for posts and basic post
       createDirectoryIfMissing True "posts"
       setCurrentDirectory "posts"
       writeFile "example-markdown.md" exampleMDPost
 
-      --Compile the LaTeX file into a PDF
-      --Could do this for every .tex file if wanted to
+      -- Compile the LaTeX file into a PDF
+      -- Could do this for every .tex file if wanted to
       writeFile "example-latex.tex" exampleTeXPost
       x <- try (readProcess "pdflatex" ["example-latex.tex"] "")
       case x of
         Left e -> do
-            let err = show (e :: IOException)
-            putStrLn "Warning: LaTeX installation not found, removing LaTeX post"
-            removeFile "example-latex.tex"
-            return "LaTeX not found"
+          let err = show (e :: IOException)
+          putStrLn "Warning: LaTeX installation not found, removing LaTeX post"
+          removeFile "example-latex.tex"
+          return "LaTeX not found"
         Right r -> return "Success"
 
       putStrLn "Success initializing!"
--- src/Heckle.hs
+++ src/Heckle.hs
@@ -48,10 +48,10 @@ data Month
   | October
   | November
   | December
-  deriving (Show, Eq)
+  deriving (Show, Eq, Bounded, Enum)
 
-month ::  Int -> Month
-month = toEnum . (-1)
+month :: Int -> Month
+month n = toEnum (n-1)
 
 displayDate :: DateTime -> String
 displayDate (DateTime y m d h mins s) =
@@ -78,7 +78,7 @@ getOutputExtension LaTeX    = ".pdf"
 getOutputExtension Markdown = ".html"
 
 -- data FileName = FileName { getFileName :: String, getExtension ::  }
-newtype Title = Title { getTitle :: String } deriving (Show, Eq, IsString)
+newtype Title = Title { getTitle :: String } deriving (Show, Eq, IsString, ToMarkup)
 
 data Post = Post
   { fileName    :: String  -- TODO make this more typed
@@ -119,10 +119,10 @@ createPost
   -> Either a Pandoc
   -> Either String Post
 createPost _ _ (Left e) = Left (show e)
-createPost format fileName (Right pd) = Post{..}
-  where
-    postTitle = getMeta docTitle pd
-    postDate  = getMeta docDate pd >>= parseAbsoluteDate
+createPost format fileName (Right pd) = do
+  postTitle <- Title <$> getMeta docTitle pd
+  postDate  <- getMeta docDate pd >>= parseAbsoluteDate
+  return Post{..}
 
 fileToPost :: String -> IO (Either String Post)
 fileToPost fileName =
@@ -133,27 +133,24 @@ fileToPost fileName =
       return . createPost Markdown name . readMarkdown def =<< readFile ("posts/" <> fileName)
     _ -> pure (Left "Not a LaTeX or MD file")
 
-injectIndex :: String -> Html -> Either String String
-injectIndex layout ul = injectAt [ (TagSoup.TagOpen "ul" [("id","blog-posts")])
-                                 , (TagSoup.TagClose "ul")]
+injectIndex :: String -> Html -> Maybe String
+injectIndex layout ul = injectAt [ TagSoup.TagOpen "ul" [("id","blog-posts")]
+                                 , TagSoup.TagClose "ul"]
                                  layout (show ul)
 
-injectTemplate :: String -> Post -> Either String String
-injectTemplate layout (Post _ _ _ Markdown pd) = injectAt tags layout inp
+injectTemplate :: String -> Post -> Maybe String
+injectTemplate layout post
+  | format post == Markdown = injectAt tags layout inp
+  | otherwise = Nothing
   where
-    tags = [(TagSoup.TagOpen "div" [("id","blog-post")]), (TagSoup.TagClose "div")]
-    inp  = "<div id='blog-post'>" <> writeHtmlString def pd <> "</div>"
+    tags = [TagSoup.TagOpen "div" [("id","blog-post")], TagSoup.TagClose "div"]
+    inp  = "<div id='blog-post'>" <> writeHtmlString def (pd post) <> "</div>"
 
 injectAt :: [TagSoup.Tag String] -> String -> String -> Maybe String
 injectAt p layout insert = case splitOn p (TagSoup.parseTags layout) of
   [beg, end] -> Just $ TagSoup.renderTags (beg <> TagSoup.parseTags insert <> end)
   _          -> Nothing
 
-writeHTML :: String -> Post -> IO ()
-writeHTML template Post{..} =
-  case format of
-    LaTeX -> return ()
-    Markdown -> do
-      case injectTemplate template p of
-        Right html -> writeFile ("posts/" <> fileName <> ".html") html
-        Left     _ -> return ()
+writeHTML :: String -> Post -> Maybe (IO ())
+writeHTML template p@Post{..} =
+  writeFile ("posts/" <> fileName <> ".html") <$> injectTemplate template p
