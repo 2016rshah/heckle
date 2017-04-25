92f929f9986c54318d77a95b0317cf33e195709a
Rushi Shah <2016rshah@gmail.com>
2017-04-25 13:25:54 -0500

    Going to upload a binary

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-
-((haskell-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-path-ghci . "stack")
-     (haskell-process-args-ghci . ("ghci"))))
- (haskell-cabal-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-path-ghci . "stack")
-          (haskell-process-args-ghci . ("ghci")))))
